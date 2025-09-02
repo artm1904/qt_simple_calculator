221b1c80ebaffb9966dc636fe47ed38de6369fb1
artm1904 <artmsavchenko@yandex.ru>
2025-09-02 19:07:34 +0300

    MVP calculator project

---
+++ src/calculator/calculator.h
@@ -0,0 +1,28 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QString>
+
+
+class Calculator
+{
+public:
+    Calculator();
+
+    int reset();
+    int digit (int d);
+    int operation(int o);
+    int calculate();
+    QString india();
+
+
+
+private:
+    double value1;
+    double value2;
+    double result;
+    int oper;
+
+};
+
+#endif // CALCULATOR_H
